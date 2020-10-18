f3204b20d24ab7f20407bbe14ee5fccf085e8950
chouzhongtian07554 <chouzhongtian07554@hellobike.com>
2020-10-18 21:12:05 +0800

    添加注释

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/Durability.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/Durability.java
@@ -28,20 +28,25 @@ import org.apache.yetus.audience.InterfaceAudience;
 public enum Durability {
   /* Developer note: Do not rename the enum field names. They are serialized in HTableDescriptor */
   /**
+   * 如果用户没有指定持久化登记，默认HBase使用SYNC_WAL等级处久化数据
    * If this is for tables durability, use HBase's global default value (SYNC_WAL).
    * Otherwise, if this is for mutation, use the table's default setting to determine durability.
    * This must remain the first option.
    */
   USE_DEFAULT,
   /**
+   * 只写缓存，不写HLog日志
+   * 因为只写内存，因此这种方式可以极大的提升写入性能，但是数据有丢失的风险
    * Do not write the Mutation to the WAL
    */
   SKIP_WAL,
   /**
+   * 异步将数据写入HLog日志中
    * Write the Mutation to the WAL asynchronously
    */
   ASYNC_WAL,
   /**
+   * 同步将数据写入日志文件中
    * Write the Mutation to the WAL synchronously.
    * The data is flushed to the filesystem implementation, but not necessarily to disk.
    * For HDFS this will flush the data to the designated number of DataNodes.
@@ -49,6 +54,7 @@ public enum Durability {
    */
   SYNC_WAL,
   /**
+   * 同步将数据写入日志文件并强制落盘
    * Write the Mutation to the WAL synchronously and force the entries to disk.
    * (Note: this is currently not supported and will behave identical to {@link #SYNC_WAL})
    * See <a href="https://issues.apache.org/jira/browse/HADOOP-6313">HADOOP-6313</a>
